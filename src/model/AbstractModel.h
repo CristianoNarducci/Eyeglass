fbe057edfb5a984868a2e26689ff4d860043c386
bitwiseFox <bitwiseFox@noemail.com>
2020-07-22 20:51:48 +0000

    Temporarely re-added erroneously deleted method. Will be moved later to controller, though

--- src/model/AbstractModel.h
+++ src/model/AbstractModel.h
@@ -24,7 +24,13 @@ public:
 	 * If the selected file is invalid, the image is discarded. 
 	 */
 	virtual void loadImage(const wxString path) = 0;
-
+	
+	/*
+	 * Loads a batch of images into the store.
+	 * Invalid files will be skipped.
+	 */
+	virtual void loadImages(const wxArrayString paths) = 0;
+	
 	
 	/*
 	 * Removes the specified image from the storage, if present.
